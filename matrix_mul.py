020eb53aa556f52a88ba048c9efb389311c51b85
Vitaliy Sayganov <vitaliysayganov@MacBook-Pro-Vitaliy.local>
2024-06-12 21:53:44 +0300

    Изменил способ приема аргументов из командной строки в main()

--- matrix_mul.py
+++ matrix_mul.py
@@ -1,21 +1,15 @@
-import argparse
+import sys
 from py_src.py_matrix import PyMatrix
 from py_src.c_lib import CLib
 
 def main():
-    arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("matrix1_path", type=str, help="Файл с первой матрицей")
-    arg_parser.add_argument("matrix2_path", type=str, help="Файл со второй матрицей")
-    arg_parser.add_argument("result_path", type=str, help="Файл для итоговой матрицы")
-    args = arg_parser.parse_args()
+    args = sys.argv
 
-    matrix_1 = PyMatrix()
-    matrix_1.readPyMatrix(args.matrix1_path)
-    matrix_2 = PyMatrix()
-    matrix_2.readPyMatrix(args.matrix2_path)
+    matrix_1 = PyMatrix().readPyMatrix(args[1])
+    matrix_2 = PyMatrix().readPyMatrix(args[2])
 
     lib = CLib('c_src/lib.dll')
-    lib.matrix_mult(matrix_1, matrix_2).writePyMatrix(args.result_path)
+    lib.matrix_mult(matrix_1, matrix_2).writePyMatrix(args[3])
 
 if __name__ == "__main__":
     main()
