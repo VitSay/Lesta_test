fa2eb2c437c514025ddc1503367308698e72896c
VitalikSay <Vitalij.Saiganov@1pt.com>
2024-06-13 10:58:07 +0300

    исправил в хэдаре все под c

--- c_src/mylib.h
+++ c_src/mylib.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <cstdint>
+#include <stdint.h>
 
 #ifdef _WIN32
 #define DLL_EXPORT __declspec(dllexport)
@@ -7,13 +7,11 @@
 #define DLL_EXPORT
 #endif
 
-extern "C" {
-    DLL_EXPORT int32_t** matrix_mult(int32_t** matrix_1,
-                                    int32_t** matrix_2,
-                                    int32_t matrix_1_n_rows,
-                                    int32_t matrix_1_n_cols,
-                                    int32_t matrix_2_n_rows,
-                                    int32_t matrix_2_n_cols);
+DLL_EXPORT int32_t** matrix_mult(int32_t** matrix_1,
+                                int32_t** matrix_2,
+                                int32_t matrix_1_n_rows,
+                                int32_t matrix_1_n_cols,
+                                int32_t matrix_2_n_rows,
+                                int32_t matrix_2_n_cols);
                                     
-    DLL_EXPORT void free_memory(int32_t** matrix, int32_t n_rows);
-}
+DLL_EXPORT void free_memory(int32_t** matrix, int32_t n_rows);
