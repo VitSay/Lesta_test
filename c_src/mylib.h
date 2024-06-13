5d7d258dee838a2ae27df0bd180bd6e1231b0405
VitalikSay <Vitalij.Saiganov@1pt.com>
2024-06-13 16:06:02 +0300

    исправил заголовчный файл, добавил константность

--- c_src/mylib.h
+++ c_src/mylib.h
@@ -7,11 +7,11 @@
 #define DLL_EXPORT
 #endif
 
-DLL_EXPORT int32_t** matrix_mult(int32_t** matrix_1,
-                                int32_t** matrix_2,
-                                int32_t matrix_1_n_rows,
-                                int32_t matrix_1_n_cols,
-                                int32_t matrix_2_n_rows,
-                                int32_t matrix_2_n_cols);
+DLL_EXPORT int32_t** matrix_mult(const int32_t** matrix_1,
+                                 const int32_t** matrix_2,
+                                 int32_t matrix_1_n_rows,
+                                 int32_t matrix_1_n_cols,
+                                 int32_t matrix_2_n_rows,
+                                 int32_t matrix_2_n_cols);
                                     
 DLL_EXPORT void free_memory(int32_t** matrix, int32_t n_rows);
